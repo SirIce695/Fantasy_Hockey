01a6ac32dcae13da91863c0730e4f1ac4687ee83
mattt <mattthom695@gmail.com>
2023-01-31 16:42:23 -0330

    Creation of player.java

---
+++ src/team.java
@@ -0,0 +1,11 @@
+public class team {
+    String teamName;
+    double teamBudget;
+    player[] players;
+
+    public team(String teamName, double teamBudget, player[] players){
+        this.teamName = teamName;
+        this.teamBudget = teamBudget;
+        this.players = players;
+    }
+}
