56c4f9345745001ae2f748a7c1e7ec853607cf03
mattt <mattthom695@gmail.com>
2023-01-31 16:45:33 -0330

    Creation of player.java for real

--- src/player.java
+++ src/player.java
@@ -1,2 +1,11 @@
 public class player {
+    String playerName;
+    int playerGoals;
+    int playerAssists;
+
+    public player(String playerName, int playerGoals, int playerAssists,){
+        this.playerName = playerName;
+        this.playerGoals = playerGoals;
+        this.playerAssists = playerAssists;
+    }
 }
